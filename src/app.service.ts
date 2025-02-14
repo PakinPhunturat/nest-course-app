2d3ef39c9bc4e69c1d46053d32d67f67d9c4bb0e
“Pakin <6730613012@psu.ac.th>
2025-02-14 10:10:15 +0700

    first commit”

---
+++ src/app.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'We Love Pay Gorn';
+  }
+
+  getName(): string {
+    return 'Buzz Popme';
+  }
+
+  getInfo(): string {
+    return 'Hello, I am Pakin Phunturat, 18 Years old';
+  }
+  
+  getJSON(){
+    return{
+      name: 'Pakin',
+      lastname: 'Phunturat',
+      age: 18,
+    };
+  }
+}
